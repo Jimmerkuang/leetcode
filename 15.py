687602c0952f2f2585600b2542e785ce72e55f77
Jimmerkuang <2017301500030@whu.edu.cn>
2022-06-03 12:38:57 +0800

    problem 15

---
+++ 15.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Time   : 2022/4/26 17:49
+# Author : kfu
+
+
+from typing import List
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+
+
+        return
+
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    answer = solution.threeSum([-1,0,1,2,-1,-4])
